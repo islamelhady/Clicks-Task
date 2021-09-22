597fe51db72ef975bbc784ebc33099a1dcaeadf5
islamelhady <i.elhady44@gmail.com>
2021-09-22 15:02:38 +0200

    add libraries

--- app/src/main/java/com/elhady/news/MainActivity.kt
+++ app/src/main/java/com/elhady/news/MainActivity.kt
@@ -3,6 +3,9 @@ package com.elhady.news
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Created by islam elhady on 22-Sep-21.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
